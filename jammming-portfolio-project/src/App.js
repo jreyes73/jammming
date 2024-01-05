2d42ba8368bbc725c34a4e7baec7aab275aeac64
Julian Reyes <julianreyes4@gmail.com>
2024-01-05 23:10:54 +0900

    first app update

--- jammming-portfolio-project/src/App.js
+++ jammming-portfolio-project/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Playlist from './Playlist';
+import SearchBar from "./SearchBar";
+import SearchResults from './SearchResults';
+import Tracklist from './Tracklist';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jammming Portfolio Project</h1>
+        <p>Scroll down for Project</p>
       </header>
+      <div>
+      <h1>Jammming Portfolio Project</h1>
+      </div>
+    <SearchBar />
+    <SearchResults></SearchResults>
+    <Playlist></Playlist>
+    <Tracklist/>
     </div>
   );
 }
